939f9d057783c882faf659958be2fa64d3ce0f07
Nguyen Van Thieu B <nguyen.van.thieub@framgias-Mac-mini-6.local>
2017-01-18 21:54:23 +0700

    Variables

---
+++ Assets/Scripts/Introduce.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Introduce : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		float speed = 2.1f;
+		double speed2 = 2.2f;
+		int a2 = 0;
+		string name = "Enemy";
+		bool isDead = true;
+		Debug.Log ("Hello");
+		int a = Random.Range(1,2);
+		Debug.Log("a= " + a);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
