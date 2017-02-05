53bb80007f734bc7ee4e40c022397e1337d87b5e
Thieu Mao <nguyen.van.thieub@framgia.com>
2017-02-05 20:43:36 +0700

    OnMouseEnter

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonManager : MonoBehaviour {
+
+	public Renderer rend;
+
+	void Start() {
+		rend = GetComponent<Renderer>();
+	}
+	void OnMouseEnter() {
+		rend.material.color = Color.red;
+		Debug.Log ("OnMouseEnter");
+	}
+//	void OnMouseOver() {
+//		rend.material.color -= new Color(0.001F, 0, 0) * Time.deltaTime;
+//		Debug.Log ("OnMouseOver");
+//	}
+//	void OnMouseExit() {
+//		rend.material.color = Color.white;
+//		Debug.Log ("OnMouseExit");
+//	}
+
+}
--- Assets/Scripts/Hello.cs
+++ Assets/Scripts/Hello.cs
@@ -6,6 +6,7 @@ public class Hello : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		Debug.Log ("Thieu Mao is here");
+		Debug.Log ("<color=red>Fatal error:</color> AssetBundle <b>not</b> found");
 	}
 	
 	// Update is called once per frame
